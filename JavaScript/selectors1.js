6d9e3c3d9e76bd28b75b8bbcaec9063d6604e367
sudhakarreddy <sudhakarreddymoola31@gmail.com>
2023-09-25 16:13:16 +0530

    Basic Selectors part in JQuery

---
+++ JavaScript/selectors1.js
@@ -0,0 +1,10 @@
+function func1()
+{
+    // $(element name).action()
+    // document.getElementByTagname(element name)
+    $("div").css('background-color','pink');
+    $("#p1").css('font-style','italic')
+    $(".para").css('font-style','italic')
+
+
+}
